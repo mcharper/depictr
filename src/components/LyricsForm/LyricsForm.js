7f778a245117215d6e69dda719269b7de15598a3
Mike Harper <mike@mcharper.com>
2018-04-07 12:51:33 +0100

    Debounce so we don't react to every character typed and slow things down

--- src/components/LyricsForm/LyricsForm.js
+++ src/components/LyricsForm/LyricsForm.js
@@ -1,12 +1,15 @@
 import React, { Component } from 'react';
 import './LyricsForm.css';
+import debounce from "lodash/debounce";
 
 class LyricsForm extends Component {
   constructor(props) {
     super(props);
     this.onChange = (event) => {
         var lyrics = event.target.value;
-        this.props.onChange(lyrics);
+        debounce(() => {
+          this.props.onChange(lyrics);
+          }, 5000)();
     }
   }
 
