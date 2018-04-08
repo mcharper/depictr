9a4dfb44ad0a2f0763653e635a0db88d5b3d0db9
Mike Harper <mike@mcharper.com>
2018-04-08 20:19:59 +0100

    Instrument so I can use Chrome Redux tab

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { createStore, applyMiddleware } from 'redux';
+import {createStore, applyMiddleware,compose} from 'redux';
 import thunkMiddleware from 'redux-thunk';
 import { Provider } from 'react-redux';  
 import registerServiceWorker from './registerServiceWorker';
@@ -8,7 +8,19 @@ import App from './App';
 import './index.css';
 import { reducer } from './reducers/index';
 
-const store = createStore(reducer, applyMiddleware(thunkMiddleware));
+const composeEnhancers =
+    typeof window === 'object' &&
+    window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ ?   
+        window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({
+        // Specify extension’s options like name, actionsBlacklist, actionsCreators, serialize...
+        }) : compose;
+
+const enhancer = composeEnhancers(
+    applyMiddleware(thunkMiddleware),
+    // other store enhancers if any
+);
+
+const store = createStore(reducer, enhancer);
 
 ReactDOM.render(
     <Provider store={store}>
